1b582668b68b6c14759cd38c1762832953c5f575
Roy Frostig <roy.frostig@gmail.com>
2012-11-25 10:34:12 -0800

    Take delimeter option in transpose.py

--- bin/transpose.py
+++ bin/transpose.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
+def print_usage():
+    print('Usage: transpose.py [-d <delim>] [file]')
+    print('')
+    print('  If no file is given, read stdin.')
+    print('')
+
 def transpose(rows):
     return zip(*rows)
 
 if __name__ == '__main__':
+    import getopt
+    import os
     import sys
 
     cmd, args = sys.argv[0], sys.argv[1:]
+
+    try:
+        opts, args = getopt.gnu_getopt(args, 'd:', [])
+    except Exception:
+        print_usage()
+        sys.exit(1)
+
+    opts = dict(opts)
+    delim = opts.get('-d')
+    join_delim = ' ' if delim is None else delim
 
     f = sys.stdin
     if len(args) > 0:
         f = open(args[0], 'r')
 
-    for y in transpose(x.split() for x in f):
-        print(' '.join(y))
+    for y in transpose(x.rstrip(os.linesep).split(delim) for x in f):
+        print(join_delim.join(y))
 
     f.close()
