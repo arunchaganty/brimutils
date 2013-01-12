9a15744b8b009d99094e3dce46a117447fea91a2
Roy Frostig <roy.frostig@gmail.com>
2013-01-12 00:46:06 -0800

    More aliases.

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,18 @@
 alias trim='sed "s/^[[:space:]]*\(.*\)[[:space:]]*$/\1/"'
 alias ltrim='sed "s/^[[:space:]]*\(.*\)$/\1/"'
 alias rtrim='sed "s/^\(.*\)[[:space:]]*$/\1/"'
+
+alias ls='ls -G' # Make ls output colorful.
+alias ll='ls -lF'
+alias la='ls -aF'
+alias lsh='ls -lshF'
+alias lsha='ls -lshaF'
+alias lsah='lsha'
+
+# git
+alias g='git'
+alias gitd='git diff'
+alias gits='git status | less'
+
+# taskwarrior
+alias t='task'
