b912a471f34562549979b16d74a01864a5f3085d
Roy Frostig <roy.frostig@gmail.com>
2013-05-20 21:40:55 -0700

    Tiny modifications to simpleplot.R

--- bin/simpleplot.R
+++ bin/simpleplot.R
@@ -10,6 +10,6 @@ d <- read.table('stdin')
 if (ncol(d) == 1) {
     d <- cbind(1:nrow(d), d)
 }
-pdf(file=paste(outname, '.pdf', sep=''), title=outname)
-plot(d, 'x', 'y', main=outname, type='o')
+pdf(file=paste(c(outname, '.pdf'), collapse=''), title=outname)
+plot(d, 'x', 'y', main=outname, type='l')
 dev.off()
